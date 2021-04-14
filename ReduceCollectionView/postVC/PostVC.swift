1cb8d92c31a84544cfa83b3e520416e0f76b8964
kimdongjune <jun36817@naver.com>
2021-04-14 15:32:37 +0900

    complete

--- ReduceCollectionView/postVC/PostVC.swift
+++ ReduceCollectionView/postVC/PostVC.swift
@@ -37,14 +37,11 @@ class PostVC: BaseViewController{
     private let imageChange = PublishSubject<Int>()
     private var index:Int = 0
     
+    private var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.timer = Observable<Int>.interval(.seconds(self.sec), scheduler: MainScheduler.instance)
-        self.timer?.filter{$0 >= 1}.subscribe {[weak self] (count) in
-            self?.imageChange.onNext(count.element!)
-            self?.imageIndex.onNext(count.element!+1)
-        }.disposed(by: disposeBag)
     }
     
     private var viewModel = PostViewModel()
@@ -59,28 +56,34 @@ class PostVC: BaseViewController{
     
     private lazy var output = viewModel.transform(input: input)
     
-    private var timer: Observable<Int>?
-    
     override func bindViewModel() {
         super.bindViewModel()
         
         output.preparingViews?.filter{$0 == true}.drive { [weak self] (bool) in
             guard let self = self else { return }
             self.view = self.postView
-            self.imageIndex.onNext(self.index)
-            self.imageChange.onNext(self.index)
-            self.index += 1
+            self.requestUpdateImage()
+            
+            self.timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(self.sec), repeats: true) { [weak self](timer) in
+                self?.requestUpdateImage()
+            }
+            
             
         }.disposed(by: disposeBag)
         
         output.updateImageSource?.drive{ [weak self] data in
-            print("check image ")
             self?.postView.imageView.image = UIImage(data: data)
             self?.imageIndex.onNext(self!.index)
             self?.index += 1
         }.disposed(by: disposeBag)
         
         
+    }
+    
+    private func requestUpdateImage(){
+        self.imageIndex.onNext(self.index)
+        self.imageChange.onNext(self.index)
+        self.index += 1
     }
     
 }
